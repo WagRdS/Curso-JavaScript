8c9caa0a5eecf52bed9faf54d526644d0fae3688
WagRdS <wagrds@users.noreply.github.com>
2024-02-26 17:18:29 -0300

    Condições em JS

--- aula11/exe008.js
+++ aula11/exe008.js
@@ -1,1 +1,6 @@
-console.log('O xonxole funfou!')
+var vel = 50
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+if (vel > 60){
+    console.log('Você ultrapassou a velocidade máxima!')
+}
+console.log('Dirija com segurança')
