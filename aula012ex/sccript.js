eb0a6504ad52fcd4612110f88be03d84e224bdea
WagRdS <wagrds@users.noreply.github.com>
2024-02-27 16:38:22 -0300

    atualização do ex012

--- aula012ex/sccript.js
+++ aula012ex/sccript.js
@@ -6,13 +6,13 @@ function carregar(){
     msg.innerHTML = `Agora são ${hora} horas.`
 
     if(hora >= 0 && hora < 12){
-        img.src = 'dia.png'
+        img.src = 'imagens/dia.png'
         document.body.style.background = '#cfed75'
     }else if(hora >= 12 && hora <= 18){
-        img.src = 'tarde.png'
+        img.src = 'imagens/tarde.png'
         document.body.style.background = '#e9a76e'
     }else {
-        img.src = 'noite.png'
+        img.src = 'imagens/noite.png'
         document.body.style.background = '#706ee9'
     }
 }
