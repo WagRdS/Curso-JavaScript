3b919ba38873aa53f42bb00b82cb9d8e70a377da
WagRdS <wagrds@users.noreply.github.com>
2024-02-28 16:18:47 -0300

    exe015

--- ex015/sccript.js
+++ ex015/sccript.js
@@ -8,13 +8,30 @@ function verificar(){
     
     res.innerHTML = `Funfou, você digitou ${Number(fano.value)}`
 
-    if(fano.value.length == 0 || Number(fano.value) > ano){
-        alert(`[ERRO]: o ano de ${ano} é inválido!`)
+    if(fano.value.length == 0 || Number(fano.value) > ano || Number(fano.value) < 1900){
+        alert(`[ERRO]: o ano de ${Number(fano.value)} é inválido! O nao deve ser no mínimo 1900`)
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `A idade é ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Masculino'
+            img.setAttribute('src', 'imagens/male.png')
+            img.setAttribute('width', '300px')
+            img.setAttribute('height', '300px')
+        } else {
+            genero = 'Feminino'
+            img.setAttribute('src', 'imagens/female.png')
+            img.setAttribute('width', '300px')
+            img.setAttribute('height', '300px')
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `O sexo escolhido foi ${genero} com ${idade} ano    `
+        res.appendChild(img)
     }
 }
+        
         
        
