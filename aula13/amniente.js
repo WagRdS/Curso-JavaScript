f5b833d4171944697ec723ba55ad004ea66ebbdd
WagRdS <wagrds@users.noreply.github.com>
2024-02-28 16:53:45 -0300

    Update amniente.js

--- aula13/amniente.js
+++ aula13/amniente.js
@@ -2,4 +2,8 @@ var i = 1
 do{
     console.log(`Passo ${i}`)
     i++
-}while( i <= 100)
+}while( i <= 10)
+console.log('--------- Outro modo')
+for(var c = 1; c <=10; c++){
+    console.log(`Exemplo ${c}`)
+}
