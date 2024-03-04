d3eb8326b43c67c1f13c9b23566aafcd0033ad0b
WagRdS <wagrds@users.noreply.github.com>
2024-03-04 16:31:33 -0300

    Exercícios
    Funções em JavaScript

---
+++ ex018/sccript.js
@@ -0,0 +1,30 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+
+}
+
+function isLista(n, l){
+    if(l.indexOf(Number(n)) == -1){
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar(){
+    if(isNumero(num.value) && isLista(lista.value, valores)){
+        alert('funfou')
+    }else{
+        alert('Número não encontrado ou já adicionado')
+    }
+}
+    
